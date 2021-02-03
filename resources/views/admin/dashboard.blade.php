1b97c4808e0257c322c6cbddef1f09b55e087f6b
KeviinAbundiiz <abundiiz_keviin@outlook.es>
2021-02-03 02:03:54 -0600

    Se agrego dasboard, sidebar y master del admin

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,65 @@
+@extends('admin.master')
+
+@section('title','Dashboard')
+
+@section('content')
+<div class="container-fluid">
+	{{-- @if(kvfj(Auth::user()->permissions, 'dashboard_small_stats')) --}}
+	<div class="panel shadow">
+		<div class="header">
+			<h2 class="title"><i class="fas fa-chart-bar"></i>	Estadísticas rápidas.</h2>
+		</div>
+	</div>
+
+	<div class="row mtop16">
+		<div class="col-md-3">
+			<div class="panel shadow">
+				<div class="header">
+					<h2 class="title"><i class="fas fa-users"></i>	Usuarios registrados.</h2>
+				</div>
+				<div class="inside">
+					<div class="big_count">0</div>
+					{{-- <div class="big_count">{{ $users }}</div> --}}
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3">
+			<div class="panel shadow">
+				<div class="header">
+					<h2 class="title"><i class="fas fa-tablet"></i>	CPU's.</h2>
+				</div>
+				<div class="inside">
+					<div class="big_count">0</div>
+					{{-- <div class="big_count">{{ $products }}</div> --}}
+				</div>
+			</div>
+		</div>
+
+		{{-- @if(kvfj(Auth::user()->permissions, 'dashboard_sell_today')) --}}
+		<div class="col-md-3">
+			<div class="panel shadow">
+				<div class="header">
+					<h2 class="title"><i class="fas fa-tv"></i>	Monitores.</h2>
+				</div>
+				<div class="inside">
+					<div class="big_count">0</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3">
+			<div class="panel shadow">
+				<div class="header">
+					<h2 class="title"><i class="fas fa-keyboard"></i>	Teclados.</h2>
+				</div>
+				<div class="inside">
+					<div class="big_count">0</div>
+				</div>
+			</div>
+		</div>
+		{{-- @endif --}}
+	</div>
+	{{-- @endif --}}
+</div>
+@endsection
---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,57 @@
+<div class="sidebar shadow">
+	<div class="section-top">
+		<div class="logo">
+			<img src="{{ url('static/images/chevrolet.png') }}" class="img-fluid">
+		</div>
+		<div class="user mtop16">
+			<span class="subtitle">Bienvenido:</span>
+			<div class="name">
+				{{ Auth::user()->name }}	{{ Auth::user()->lastname }}
+				<a href="{{ url('/logout') }}" data-toggle="tooltip" data-placement="top" title="Cerrar Sesión">
+					<i class="fas fa-sign-out-alt"></i>
+				</a>
+			</div>
+			<div class="email">{{ Auth::user()->email }}</div>
+		</div>
+	</div>
+
+	<div class="main">
+		<ul>
+			{{-- @if(kvfj(Auth::user()->permissions, 'dashboard')) --}}
+			<li>
+				<a href="{{ url('/admin') }}" class="lk-dashboard"><i class="fas fa-home"></i>Inicio</a>
+			</li>
+			{{-- @endif --}}
+			{{-- @if(kvfj(Auth::user()->permissions, 'categories')) --}}
+			{{-- <li>
+				<a href="{{ url('/admin/categories/0') }}" class="lk-categories lk-category_add lk-category_edit lk-category_delete"><i class="fas fa-folder-open"></i>Categorías</a>
+			</li> --}}
+			{{-- @endif --}}
+			{{-- @if(kvfj(Auth::user()->permissions, 'products')) --}}
+			{{-- <li>
+				<a href="{{ url('/admin/products/1') }}" class="lk-products lk-product_add lk-product_search lk-product_edit lk-product_gallery_add"><i class="fas fa-boxes"></i>Productos</a>
+			</li> --}}
+			{{-- @endif --}}
+			{{-- @if(kvfj(Auth::user()->permissions, 'orders_list')) --}}
+			{{-- <li>
+				<a href="{{ url('/admin/orders/all') }}" class="lk-orders_list"><i class="fas fa-clipboard-list"></i>Órdenes</a>
+			</li> --}}
+			{{-- @endif --}}
+			{{-- @if(kvfj(Auth::user()->permissions, 'user_list')) --}}
+			<li>
+				<a href="{{ url('/admin/users/all') }}" class="lk-user_list lk-user_edit lk-user_permissions lk-user_banned"><i class="fas fa-user-friends"></i>Usuarios</a>
+			</li>
+			{{-- @endif --}}
+			{{-- @if(kvfj(Auth::user()->permissions, 'sliders_list')) --}}
+			{{-- <li>
+				<a href="{{ url('/admin/sliders') }}" class="lk-sliders_list lk-slider_edit"><i class="fas fa-images"></i>Sliders</a>
+			</li> --}}
+			{{-- @endif --}}
+			{{-- @if(kvfj(Auth::user()->permissions, 'settings')) --}}
+			{{-- <li>
+				<a href="{{ url('/admin/settings') }}" class="lk-settings"><i class="fas fa-cogs"></i>Configuraciones</a>
+			</li> --}}
+			{{-- @endif --}}
+		</ul>
+	</div>
+</div>
